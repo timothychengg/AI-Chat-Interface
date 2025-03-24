ec77b3832e622ba0186657197aebeab65242624c
Timothy <teecheng5@gmail.com>
2025-03-24 14:50:02 -0700

    Fix: Add missing React imports to _app.js and _document.js for JSX support

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
-      <body className="antialiased">
+      <body className='antialiased'>
         <Main />
         <NextScript />
       </body>
