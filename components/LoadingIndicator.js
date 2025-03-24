df80c58e00802c8bcaddbc19c03d3f51163022c3
Timothy <teecheng5@gmail.com>
2025-03-24 16:08:58 -0700

    Refactor: Optimize LoadingIndicator with cleaner dynamic animation delays

--- components/LoadingIndicator.js
+++ components/LoadingIndicator.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
 export default function LoadingIndicator() {
+  const delays = [0, 200, 400];
+
   return (
     <div className='flex items-center space-x-2 text-sm text-gray-500 px-4'>
-      <span className='relative flex h-2 w-2'>
-        <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-400 opacity-75'></span>
-        <span className='relative inline-flex rounded-full h-2 w-2 bg-blue-500'></span>
-      </span>
-      <span className='relative flex h-2 w-2'>
-        <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-400 opacity-75 animation-delay-200'></span>
-        <span className='relative inline-flex rounded-full h-2 w-2 bg-blue-500'></span>
-      </span>
-      <span className='relative flex h-2 w-2'>
-        <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-400 opacity-75 animation-delay-400'></span>
-        <span className='relative inline-flex rounded-full h-2 w-2 bg-blue-500'></span>
-      </span>
+      {delays.map((delay, idx) => (
+        <span key={idx} className='relative flex h-2 w-2'>
+          <span
+            className='animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-400 opacity-75'
+            style={{ animationDelay: `${delay}ms` }}
+          ></span>
+          <span className='relative inline-flex h-2 w-2 rounded-full bg-blue-500'></span>
+        </span>
+      ))}
       <span>AI is typing...</span>
     </div>
   );
